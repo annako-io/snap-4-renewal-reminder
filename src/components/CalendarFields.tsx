e14e8d6cc7c8a213a1f0a2bbfa757f720b301d72
annako-io <annako1@icloud.com>
2024-06-16 18:48:35 -0700

    ts-CalendarFields

--- src/components/CalendarFields.tsx
+++ src/components/CalendarFields.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { ReactElement, Dispatch, SetStateAction } from 'react';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import EventDateTimePicker from './DateTimePicker';
 
-const CalendarFields = ({ start, changeStart, end, changeEnd, eventName, eventDescription, setEventName, setEventDescription }) => {
+// Props type
+type PropsType = {
+  start: string,
+  changeStart: Dispatch<SetStateAction<string>>,
+  end: string,
+  changeEnd: Dispatch<SetStateAction<string>>,
+  eventName: string,
+  eventDescription: string,
+  setEventName: Dispatch<SetStateAction<string>>,
+  setEventDescription: Dispatch<SetStateAction<string>>
+};
+
+const CalendarFields = ({ start, changeStart, end, changeEnd, eventName, eventDescription, setEventName, setEventDescription }: PropsType): ReactElement => {
 
   return (
     <Stack spacing={2} justifyContent="center">
